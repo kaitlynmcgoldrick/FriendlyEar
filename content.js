46c4f8fc040055b2189d323abd5c1bec9d12c8b6
Kaitlyn McGoldrick <mcgoldrick.kaitlyn@gmail.com>
2018-05-26 18:29:20 -0400

    Http request

--- content.js
+++ content.js
@@ -1,11 +1,39 @@
 showHide();
 
 function showHide() {
+    var typingTimer;    
+    var doneTypingInterval = 1000;  
+
+        
     window.addEventListener("keyup", () => {
-        console.log("listening")
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);
     })
 
+    window.addEventListener("keydown", () => {
+        clearTimeout(typingTimer);
 
+    })
+
+    function doneTyping () {
+        console.log("done typing");
+        let focused = document.activeElement
+        // let myValue = document.querySelector('input').value;
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://192.34.78.218/api/inputUserData', true);
+        // xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        // xhr.onreadystatechange = handler;
+        xhr.send(JSON.stringify({
+            "time": 3234234,
+            "text": "fhdkfhsdkfj"
+        })); 
+        console.log(focused.value);
+
+        // var request = makeHttpObject();
+        // request.open('POST', 'https://192.34.78.218/api/inputUserData', false);
+        // request.send(JSON.stringify({}));
+        // print(request.responseText);
+      }
     
     var el = document.getElementById("comments");
     if( el && el.style.display == 'none')
